9f998a0b2b109f7cbd7d776959c84542203217f7
fredkeemhaus <fredkeemhaus@gmail.com>
2022-02-15 21:27:24 +0900

    server connect

--- src/config/const.js
+++ src/config/const.js
@@ -2,5 +2,5 @@
 export default {
   // HOST: "http://localhost:8000", // LOCAL/
   // HOST: "http://18.116.38.147:8000/",
-  HOST: "https://d2ri7cyfi98bjj.cloudfront.net/",
+  HOST: "https://server.dev-team-aim.com",
 };
