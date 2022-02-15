a50f648ab1b451370670898690cbdda172e1bd06
fredkeemhaus <fredkeemhaus@gmail.com>
2022-02-15 20:59:20 +0900

    check fred

--- src/config/const.js
+++ src/config/const.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   // HOST: "http://localhost:8000", // LOCAL/
-  HOST: "http://18.116.38.147:8000/",
+  // HOST: "http://18.116.38.147:8000/",
+  HOST: "https://d2ri7cyfi98bjj.cloudfront.net/",
 };
