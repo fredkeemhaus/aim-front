9391ca5d84c68f7d80422a88cb24d17b1d227a47
fredkeemhaus <fredkeemhaus@gmail.com>
2022-02-15 22:07:38 +0900

    delete cors

--- src/api/axios-instance.js
+++ src/api/axios-instance.js
@@ -4,14 +4,10 @@ import { localGetItem } from "../utils/handleStorage";
 
 const instance = axios.create({
   baseURL: CONST.HOST,
-  withCredentials: true,
 });
 
 instance.defaults.headers.post["Content-Type"] =
   "application/x-www-form-urlencoded";
-
-instance.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
-instance.defaults.headers.common["Access-Control-Allow-Credentials"] = true;
 
 instance.interceptors.request.use(
   async (response) => {
